29628b212021f928b97a5532b2e09ca1729f7762
Oyama <godstimeoyama082@gmail.com>
2023-12-27 21:08:09 +0100

    new stuffs

---
+++ Desktop/EXPERIMENT/test.js
@@ -0,0 +1,21 @@
+ 
+// console.log('Hello');
+
+// alert("PLATE");
+// function greetz(){
+//     let greets = 'Hello, World';
+//     console.log(greets + " hello hi ");
+
+
+// }
+// greetz();
+let g=1;
+const t= function(){
+    g=2;
+};
+console.log(g);
+t();
+console.log(g);
+
+
+
